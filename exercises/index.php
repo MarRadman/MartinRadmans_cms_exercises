2627e97495f644dabd61bd6e8f4410a186ef2aa5
Martin Rådmans <126577368+MarRadman@users.noreply.github.com>
2024-11-11 15:10:11 +0100

    Development (#2)
    * 2nd commit
    
    * First exercie done
    
    ---------
    
    Co-authored-by: Martin Rådmans <mcflander@MacBook-Pro-som-tillhor-Martin.local>

--- exercises/index.php
+++ exercises/index.php
@@ -1,3 +1,28 @@
+<!-- Övning 1
+- skapa två variabler och en array med minst tre
+lagrade värden.
+- Skriv ut en mening till skärmen innehållandes
+båda dina variabler och alla värden i arrayen i
+valfri ordning. Blanda utskriften med hårdkodad
+text så att meningen blir sammanhängande.
+
+Övning 2
+- Skapa en funktion is_prime() Som räknar ut
+om ett tal är ett primtal. (Primtal är ett nummer
+som är större än ett och som inte kan delas
+jämnt med något annat tal än 1 eller sig själv.)
+
+Övning 3
+- Skapa en funktion som tar emot en associativ
+array med nycklarna "bord", "frukt" och "extra".
+Den ska returnera en sträng som förklarar för
+en juice-bar-servitör vart en beställning ska,
+vilka frukter den ska innehålla samt, om det
+finns en extrabeställning även skriva vad den
+innehåller.
+- Du får gärna låta värdet för frukt vara en egen
+array -->
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,14 +34,11 @@
 
 <body>
   <?php
-  $name = "John";
-  $age = 25;
-  $isMale = true;
-  $height = 1.85;
-  $salary = null;
-
-  echo "Name: $name <br>";
-  echo "Age: $age <br>";
+  // Övning 1
+  $a = 1;
+  $b = 2;
+  $c = [3, 4, 5];
+  echo "Värde a: $a, värde b: $b, värde c: $c[0], $c[1], $c[2]";
   ?>
 </body>
 
