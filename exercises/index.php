34afd5c8fa02e59b439debd2f6ae42fe07f34d61
Martin Rådmans <mcflander@MacBook-Pro-som-tillhor-Martin.local>
2024-11-11 14:55:50 +0100

    2nd commit

---
+++ exercises/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  $name = "John";
+  $age = 25;
+  $isMale = true;
+  $height = 1.85;
+  $salary = null;
+
+  echo "Name: $name <br>";
+  echo "Age: $age <br>";
+  ?>
+</body>
+
+</html>
