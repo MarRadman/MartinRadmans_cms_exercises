d95a3b08c0bab10cd02833d47a6aacace1e929d0
Martin Rådmans <mcflander@MacBook-Pro-som-tillhor-Martin.local>
2024-11-13 11:12:18 +0100

    Finished exercise 1,2,3

--- exercises/index.php
+++ exercises/index.php
@@ -1,28 +1,3 @@
-<!-- Övning 1
-- skapa två variabler och en array med minst tre
-lagrade värden.
-- Skriv ut en mening till skärmen innehållandes
-båda dina variabler och alla värden i arrayen i
-valfri ordning. Blanda utskriften med hårdkodad
-text så att meningen blir sammanhängande.
-
-Övning 2
-- Skapa en funktion is_prime() Som räknar ut
-om ett tal är ett primtal. (Primtal är ett nummer
-som är större än ett och som inte kan delas
-jämnt med något annat tal än 1 eller sig själv.)
-
-Övning 3
-- Skapa en funktion som tar emot en associativ
-array med nycklarna "bord", "frukt" och "extra".
-Den ska returnera en sträng som förklarar för
-en juice-bar-servitör vart en beställning ska,
-vilka frukter den ska innehålla samt, om det
-finns en extrabeställning även skriva vad den
-innehåller.
-- Du får gärna låta värdet för frukt vara en egen
-array -->
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -33,13 +8,18 @@ array -->
 </head>
 
 <body>
-  <?php
-  // Övning 1
-  $a = 1;
-  $b = 2;
-  $c = [3, 4, 5];
-  echo "Värde a: $a, värde b: $b, värde c: $c[0], $c[1], $c[2]";
-  ?>
+  <h1>Exercises</h1>
+  <hr>
+  <h2>Exercise 1</h2>
+  <?php include 'exercise1.php'; ?>
+  <hr>
+  <h2>Exercise 2</h2>
+  <p>Number 7 is prime?</p>
+  <?php include 'exercise2.php'; ?>
+  <hr>
+  <h2>Exercise 3</h2>
+  <?php include 'exercise3.php'; ?>
+  <hr>
 </body>
 
 </html>
